39020857bd5f5c853128e317488abbb13c7dbf4b
Shruti211431 <shruti.211431.it@mhssce.ac.in>
2024-06-24 12:24:34 +0530

    main method

---
+++ project3/src/com/tnisf/daythree/SingleInheritance.java
@@ -0,0 +1,13 @@
+package com.tnisf.daythree;
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st = new Student("Shruti", 123467, "Parel, Mumbai", 1234567890, 20, "M.H> Saboo Siddik College");
+//		st.setRollNo(21);
+//		st.setCollegeName("M.H. Saboo Siddik College");
+		System.out.println(st);
+	}
+
+}
--- project3/src/com/tnisf/daythree/Student.java
+++ project3/src/com/tnisf/daythree/Student.java
@@ -7,13 +7,29 @@ public class Student extends Citizen{
 		super();
 		// TODO Auto-generated constructor stub
 	}
-//	public Student(String name, long adhaarNo, String address, long phone) {
-//		super(name, adhaarNo, address, phone);
-//		// TODO Auto-generated constructor stub
-//	}
-	public Student(int rollNo, String collegeName) {
-		super(name, adhaarNo, address, phone);// calling the parameterized constructor
+	public Student(String name, long adhaarNo, String address, long phone, int rollN, String collegeName) {
+		super(name, adhaarNo, address, phone);
+		// TODO Auto-generated constructor stub
+		this.rollNo=rollNo;
+		this.collegeName=collegeName;
+	}
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
 		this.rollNo = rollNo;
+	}
+	public String getCollegeName() {
+		return collegeName;
+	}
+	public void setCollegeName(String collegeName) {
 		this.collegeName = collegeName;
+	}
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", collegeName=" + collegeName + ", getName()=" + getName()
+				+ ", getAdhaarNo()=" + getAdhaarNo() + ", getAddress()=" + getAddress() + ", getPhone()=" + getPhone()
+				+ ", toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ "]";
 	}
 }
