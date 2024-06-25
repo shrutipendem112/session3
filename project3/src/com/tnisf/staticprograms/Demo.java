5bc3f4f34848f33a65f9668185fd50406a0aa81b
Shruti211431 <shruti.211431.it@mhssce.ac.in>
2024-06-26 01:27:29 +0530

    static programs day four

--- project3/src/com/tnisf/staticprograms/Demo.java
+++ project3/src/com/tnisf/staticprograms/Demo.java
@@ -4,9 +4,14 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Employee.companyName; //calling static variable
 		Employee ob = new Employee("Shruti", 21); //Calling instance variable using constructor
 		System.out.println("Company" + Employee.companyName);
 		System.out.println(ob);
+		
+		System.out.println();
+		System.out.println("FLOW OF EXECUTION");
+		MyClass mc = new MyClass();
+		MyClass.display();
+		System.out.println("Section is " + mc.getSection());
 	}
 }
--- project3/src/com/tnisf/staticprograms/MyClass.java
+++ project3/src/com/tnisf/staticprograms/MyClass.java
@@ -1,20 +1,33 @@
 package com.tnisf.staticprograms;
 
 public class MyClass {
-	private int section; // instance variable
+	private int section = 50; // instance variable
+	public int getSection() {
+		return section;
+	}
+	public void setSection(int section) {
+		this.section = section;
+	}
 	private static int srNo; //static variable
 	
 	static  {
-		System.out.println("Staic Block");
+		System.out.println("Static Block");
 		srNo = 100;
 	}
 	
-	MyClass{
-		
+	MyClass(){
+		System.out.println("------Default Contructor------");
+		srNo++;
+		section++;
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static void display() {
+		System.out.println("Serial No. is " +MyClass.srNo);
 	}
+	
+	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
 
 }
