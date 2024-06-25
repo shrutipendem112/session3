d295e201d645fe0b8b7fea5423bab5575766b989
Shruti211431 <shruti.211431.it@mhssce.ac.in>
2024-06-25 21:33:51 +0530

    updated

--- assignment4/src/com/tnisf/booking/MovieMain.java
+++ assignment4/src/com/tnisf/booking/MovieMain.java
@@ -4,10 +4,10 @@ public class MovieMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TicketBooking tb1 = new TicketBooking("Shruti", "The Crow", 9);
+		TicketBooking tb1 = new TicketBooking("Shruti");
 		System.out.println(tb1);
 		
-		TicketBooking tb2 = new TicketBooking("John", "The Crow", 9);
+		TicketBooking tb2 = new TicketBooking("John");
 		System.out.println(tb2);
 	}
 
--- assignment4/src/com/tnisf/booking/TicketBooking.java
+++ assignment4/src/com/tnisf/booking/TicketBooking.java
@@ -4,71 +4,38 @@ public class TicketBooking {
 	
 	//instance variable
 	private String name;
-	private String movieName;
-	private int time;
 	
 	
 	//static variable to track the number of tickets
 	static int ticket = 10;
+	static String movieName= "The Crow";
+	static int time = 9;
 	
+	
+	// static method which calls static variable
+	static void remainingSeats() {
+		ticket--; // decrementing number of ticket using static variable
+		System.out.println("Ticket booked successfully");
+	}
+	
+	static {
+		System.out.println("-------Welcome to Booking center-------");
+	}
 
-	public TicketBooking(String name, String movieName, int time) {
+	public TicketBooking(String name) {
 		if (ticket > 0)
 		{
 			this.name = name;
-			this.movieName = movieName;
-			this.time = time;
-			ticket--; // decrementing number of ticket
-			System.out.println("Ticket booked successfully");
+			TicketBooking.remainingSeats();
+			
 		}else {
 			System.out.println("No available tickets");
 		}
 	}
 
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public String getMovieName() {
-		return movieName;
-	}
-
-
-	public void setMovieName(String movieName) {
-		this.movieName = movieName;
-	}
-
-
-	public int getTime() {
-		return time;
-	}
-
-
-	public void setTime(int time) {
-		this.time = time;
-	}
-
-
-	public static int getTicket() {
-		return ticket;
-	}
-
-
-	public static void setTicket(int ticket) {
-		TicketBooking.ticket = ticket;
-	}
-
-
 	@Override
 	public String toString() {
-		return "TicketBooking [name=" + name + ", movieName=" + movieName + ", time=" + time + ", Available tickets= " + TicketBooking.ticket +"]";
+		return "TicketBooking [name=" + name + ", movieName=" + TicketBooking.movieName + ", time=" + TicketBooking.time + "pm, Available tickets= " + TicketBooking.ticket +"]";
 	}
 
 }
